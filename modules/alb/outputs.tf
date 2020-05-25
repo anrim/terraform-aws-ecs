1545dde3f93a19e0aed2bd99912b52c37112267d
Josh Hudson <jjhudson1@gmail.com>
2020-05-25 15:14:53 -0600

    Update https listener and target_group outputs to match updated terraform-aws-alb

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,10 +1,10 @@
 output "listener_https_arn" {
   description = "The ARN of the HTTPS ALB Listener that can be used to add rules"
-  value       = module.alb.alb_listener_https_arn
+  value       = module.alb.https_listener_arns[0]
 }
 
 output "target_group_arn" {
   description = "ARN of the target group"
-  value       = module.alb.target_group_arn
+  value       = module.alb.target_group_arns[0]
 }
 
